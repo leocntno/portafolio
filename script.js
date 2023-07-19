fcfb08b9464c8088d54ca231b5892cc2de619991
leo <leocenteno8@outlook.com>
2023-07-19 14:59:44 -0600

    Proyectos y estilos agregados

--- script.js
+++ script.js
@@ -11,10 +11,18 @@ function toggleMenu() {
 ScrollReveal().reveal('.container');
 
 const buttons = document.querySelectorAll('.read-more-btn');
+const buttonsForm = document.querySelectorAll('.read-more-btn_Form');
 const textElements = document.querySelectorAll('.pres');
+const textElementsForm = document.querySelectorAll('.presForm');
 
 buttons.forEach((button, index) => {
   button.addEventListener('click', () => {const textElement = textElements[index];
     textElement.classList.toggle('showText');
     ScrollReveal().reveal('.pres', {delay: 100});});
+  });
+
+  buttonsForm.forEach((buttonForm, index) => {
+    buttonForm.addEventListener('click', () => {const textElementForm = textElementsForm[index];
+      textElementForm.classList.toggle('showTextForm');
+      ScrollReveal().reveal('.presForm', {delay: 100});});
   });
