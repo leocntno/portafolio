f79a6bcd551f16915a0b3d036a49d853ce492304
leo <leocenteno8@outlook.com>
2023-07-26 15:38:23 -0600

    Se eliminó la parte del script no necesaria

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function toggleMenu() {
-    var menuItems = document.querySelector(".headerPrincipal-nav");
-    if (menuItems.style.display == "none"){
-        menuItems.style.display = "flex";
-      }else{
-        menuItems.style.display = "none";
-      }
-  }
-
 document.querySelector(".contenedorCertificados").addEventListener("click",function(){
   document.querySelector(".contenedorCertificados ul").classList.toggle("show");
 })
