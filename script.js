212b5c5dc148400f3b3a72db82fea2e781972b6d
leo <leocenteno8@outlook.com>
2023-07-27 15:12:43 -0600

    Version 1.1, se agrega boton para desplegar menu v2.0

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+//CODIGO BOTON MENU
+
+const btnMenu = document.querySelector(".btnHeader")
+const menu = document.querySelector(".headerBar nav")
+
+  btnMenu.addEventListener('click', ()=>{
+      menu.classList.toggle('show')
+  });
+
+//CODIGO INFORMACION DESPLEGABLE
+
 document.querySelector(".contenedorCertificados").addEventListener("click",function(){
   document.querySelector(".contenedorCertificados ul").classList.toggle("show");
-})
-
+});
 
 document.querySelector(".contenedorProyecto").addEventListener("click",function(){
   document.querySelector(".contenedorProyecto h2").classList.toggle("show");
-})
-
+});
 
 document.querySelector(".contenedorProyectoDos").addEventListener("click",function(){
   document.querySelector(".contenedorProyectoDos h2").classList.toggle("show");
-})
+});
