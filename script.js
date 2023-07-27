c095480eb92c0f76521f67541e3d2819bd89a0f3
leo <leocenteno8@outlook.com>
2023-07-27 14:52:27 -0600

    Version 1.1, se quitó la parte del codigo provada que no funciona

--- script.js
+++ script.js
@@ -11,12 +11,3 @@ document.querySelector(".contenedorProyecto").addEventListener("click",function(
 document.querySelector(".contenedorProyectoDos").addEventListener("click",function(){
   document.querySelector(".contenedorProyectoDos h2").classList.toggle("show");
 })
-
-
-const toggleBtn = document.getElementsByClassName('btnHeader');
-const menu = document.getElementById('menu');
-
-toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('visible');
-  menu.classList.toggle('hidden');
-});
