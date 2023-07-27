69b327651d0a5adeb53786c4574585d310bde5dc
leo <leocenteno8@outlook.com>
2023-07-27 14:24:47 -0600

    Version 1.1, se intenta implementar un boton

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ document.querySelector(".contenedorProyecto").addEventListener("click",function(
 document.querySelector(".contenedorProyectoDos").addEventListener("click",function(){
   document.querySelector(".contenedorProyectoDos h2").classList.toggle("show");
 })
+
+
+const toggleBtn = document.getElementsByClassName('btnHeader');
+const menu = document.getElementById('menu');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('visible');
+  menu.classList.toggle('hidden');
+});
